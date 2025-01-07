2391be23143fb42fb98de473b8ddc43e4158e0d8
PranavFaladu <fadadupranav@gmail.com>
2025-01-07 13:34:21 +0530

    new file add

---
+++ data/main.js
@@ -0,0 +1,59 @@
+// core modules 
+
+
+// const nameofmodule = require('nameofmodule')
+
+const fs = require("fs")
+
+// extenal modules
+//local modules - created by us 
+
+// create a directory 
+
+
+// fs.mkdirSync("data")
+
+
+// remove the directory
+
+// fs.rmdirSync("projects")
+
+// create a file 
+
+// let data = "hello how are you"
+// fs.writeFileSync("info.txt",data)
+
+
+// let data2 = "\n hello how are you pranav faladu"
+// fs.writeFileSync("info.txt",data2)
+
+
+// let data2 = "\n hello how are you princy"
+// fs.appendFileSync("info.txt",data2)
+
+// fs.writeFileSync("demo.txt","hello")
+
+// fs.unlinkSync("demo.txt")
+
+// let data2 = "\n it feels we are domn good at it hello how are you"
+// output_path = "node.txt"
+// fs.appendFileSync(output_path,data2,(err)=>{
+//     if(err){
+//         throw err
+//     }
+//     console.log(`successfully wrore data to ${output_path}`)
+// })
+
+// let = x
+// let input_path = "node.txt"
+// // let data = fs.readFileSync("node.txt").toString()
+// // console.log(data)
+
+// fs.readFile(input_path,(err,data)=>{
+//     if(err){
+//         throw err
+//     }
+    
+//     console.log(data.toString())
+//     console.log(`file read successfully ${input_path}`)
+// })
